9b939593d38cff2c88da850e78595b85ce61dae2
GammaG <m_seidler@hotmail.de>
2014-04-15 16:44:33 +0200

    bla

--- src/sm/htw/main/HelloSematicWeb.java
+++ src/sm/htw/main/HelloSematicWeb.java
@@ -9,9 +9,16 @@ import com.hp.hpl.jena.query.QueryExecutionFactory;
 import com.hp.hpl.jena.query.QueryFactory;
 import com.hp.hpl.jena.query.QuerySolution;
 import com.hp.hpl.jena.query.ResultSet;
+import com.hp.hpl.jena.rdf.model.InfModel;
 import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
+import com.hp.hpl.jena.rdf.model.Property;
 import com.hp.hpl.jena.rdf.model.RDFNode;
+import com.hp.hpl.jena.rdf.model.Resource;
+import com.hp.hpl.jena.reasoner.Reasoner;
+import com.hp.hpl.jena.reasoner.ReasonerRegistry;
+import com.hp.hpl.jena.reasoner.rulesys.GenericRuleReasoner;
+import com.hp.hpl.jena.reasoner.rulesys.Rule;
 import com.hp.hpl.jena.util.FileManager;
 
 public class HelloSematicWeb {
@@ -20,6 +27,7 @@ public class HelloSematicWeb {
 
 	private Model friends = null;
 	private Model schema = null;
+	private InfModel inferredFriends;
 
 	public static void main(String[] args) throws IOException {
 		HelloSematicWeb hsw = new HelloSematicWeb();
@@ -30,9 +38,13 @@ public class HelloSematicWeb {
 		System.out.println("Add the new Ontologies");
 		hsw.populateFOAFSchema();
 		hsw.populateNewFriendsSchema();
+		System.out.println("Define Ontologie relations");
+		hsw.createRelationsToOntologies();
+		System.out.println("Run a reasoner");
+		hsw.bindReasoner();
 		System.out.println("Say Hello to Myself");
 		hsw.mySelf(hsw.friends);
-		System.out.println("Say Hello to my FOAF Friends");
+		System.out.println("Say Hello to my Friends");
 		hsw.myFriends(hsw.friends);
 
 	}
@@ -62,7 +74,8 @@ public class HelloSematicWeb {
 		// Now add query
 		queryStr.append(queryRequest);
 		Query query = QueryFactory.create(queryStr.toString());
-		QueryExecution qexec = QueryExecutionFactory.create(query, model);
+		QueryExecution qexec = QueryExecutionFactory.create(query,
+				inferredFriends);
 
 		// Run Select
 		try {
@@ -107,9 +120,55 @@ public class HelloSematicWeb {
 
 	private void populateNewFriendsSchema() throws IOException {
 		InputStream inFoafInstance = FileManager.get().open(
-				"rdf/IndividualsOntologies.owl");
+				"rdf/IndividualsO.owl");
 		friends.read(inFoafInstance, defaultNameSpace);
 		inFoafInstance.close();
+	}
+
+	private void createRelationsToOntologies() {
+		// State that :individuals is equivalentClass of foaf:Person
+		Resource resource = schema.createResource(defaultNameSpace
+				+ "Individual");
+		Property prop = schema.createProperty("owl:equivalentClass");
+		Resource obj = schema.createResource("foaf:Person");
+		schema.add(resource, prop, obj);
+
+		// State that :hasName is equivalentProperty of foaf:name
+		resource = schema.createResource(defaultNameSpace + "hasName");
+		prop = schema.createProperty("owl:equivalentProperty");
+		obj = schema.createResource("foaf:name");
+		schema.add(resource, prop, obj);
+
+		// State that :hasFriend is a subproperty of foaf:knows
+		resource = schema.createResource(defaultNameSpace + "hasFriend");
+		prop = schema.createProperty("rdfs:subPropertyOf");
+		obj = schema.createResource("foaf:knows");
+		schema.add(resource, prop, obj);
+
+		// State that sem web is the same person as Semantic Web
+		resource = schema.createResource("swp2: #me");
+		prop = schema.createProperty("owl:sameAs");
+		obj = schema.createResource("http://swp2:#Indi_5");
+		schema.add(resource, prop, obj);
+
+	}
+
+	private void bindReasoner() {
+		Reasoner reasoner = ReasonerRegistry.getOWLReasoner();
+		// Reasoner reasoner = PelletReasonerFactory.theInstance().create();
+		reasoner = reasoner.bindSchema(schema);
+		inferredFriends = ModelFactory.createInfModel(reasoner, friends);
+		runJenaRule(inferredFriends);
+	}
+
+	private void runJenaRule(Model model) {
+		String rules = " [emailChange:"
+				+ "(?person foaf:mbox ?email),strConcat(?email,?lit),"
+				+ "regex( ?lit, \"(.*@gmail.com)')"
+				+ " -> (?person rdf:type> People:GmailPerson)]";
+		Reasoner ruleReasoner = new GenericRuleReasoner(Rule.parseRules(rules));
+		ruleReasoner = ruleReasoner.bindSchema(schema);
+		inferredFriends = ModelFactory.createInfModel(ruleReasoner, model);
 	}
 
 }
